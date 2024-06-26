15d130e009041293cbc6cbb814608e0befe00d13
mahemedaziz <azizberrais@outlook.com>
2024-06-26 10:10:28 +0100

    logo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -68,11 +68,11 @@ export default function Login() {
     <>
       <ToastContainer />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="flex sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             src={robot}
             alt="hoobank"
-            className="w-[80.14px] h-[100.14px] object-contain "
+            className="w-[266px] h-[172.14px] object-contain ml-12 "
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">
             Sign in to your account
